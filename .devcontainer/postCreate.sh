82404a70f545a0a0a4f33fb61f6d33e81efda588
Remco Brosky <remcobrosky@microsoft.com>
2024-02-28 13:29:14 +0100

    devcontainer postcreate

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 chmod u+x ./scripts/set-env.sh
+chmod u+x ./scripts/appreg.sh
+chmod u+x ./scripts/cleanup.sh
+chmod u+x ./scripts/deploy-azurechat.sh
+chmod u+x ./scripts/set-az-currentsubscription.sh

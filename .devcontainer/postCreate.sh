f89ca754287575ea24dafd6c075b61b84cc619a7
Remco Brosky <remco@brosky.net>
2024-03-19 15:40:17 +0000

    Linefeeds

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,3 +4,4 @@ chmod u+x ./scripts/appreg.sh
 chmod u+x ./scripts/cleanup.sh
 chmod u+x ./scripts/deploy-azurechat.sh
 chmod u+x ./scripts/set-az-currentsubscription.sh
+curl -fsSL https://aka.ms/install-azd.sh | bash
